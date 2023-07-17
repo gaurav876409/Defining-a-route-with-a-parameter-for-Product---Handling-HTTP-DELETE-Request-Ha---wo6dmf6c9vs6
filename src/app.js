5ec8f74484f1b8fa35faa0ee3e201ab8ef2e7753
gaurav876409 <gaurav876409@gmail.com>
2023-07-17 19:04:20 +0530

    up

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.get('/product/:productId', (req, res) => {
   //Write a code here for endpoint /product/:productId and also print parameter in json form
+  const productId = req.params.productId;
+
+  // Return the productId in JSON format
+  res.status(200).json({
+    productId: productId,
+  });
 });
 
 module.exports = app;
